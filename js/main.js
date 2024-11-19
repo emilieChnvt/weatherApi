c98d4acfcec58486b2a9c5cdd40e6306e349e903
Emilie <emilie.chnvt@gmail.com>
2024-11-19 18:49:34 +0100

    lat / lon added but not lyon

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const apiKey = '7e583338fd9960bbe1d8e817bc0e46e5'
-let url='https://api.openweathermap.org/data/2.5'
+const apiKey = '7e583338fd9960bbe1d8e817bc0e46e5\n'
+let url='https://api.openweathermap.org/data/2.5';
 
 
 
 
-fetch(url)
+fetch(`${url}/weather?lat=44.34&lon=10.99&appid=${apiKey}`)
     .then(response => response.json())
     .then(json => console.log(json));
